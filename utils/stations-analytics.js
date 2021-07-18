0e0e14ff2a11e2a00662c2a350c33d34214bbf1e
Glitch (fishy-careful-hiss) <none>
2021-07-18 10:00:54 +0000

    🦃📡 Checkpoint ./models/playlist-store.json:32708987/146 ./utils/logger.js:32708987/15 ./utils/stations-analytics.js:32708987/172

--- utils/stations-analytics.js
+++ utils/stations-analytics.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const playlistAnalytics = {
+const stationsAnalytics = {
 
-  getShortestSong(playlist) {
-    let shortestSong = null;
-    if (playlist.songs.length > 0) {
-      shortestSong = playlist.songs[0];
+  getNewestReading(station) {
+    let newestReading = null;
+    if (station.readings.length > 0) {
+      newestReading = station.readings[0];
       for (let i = 1; i < playlist.songs.length; i++) {
         if (playlist.songs[i].duration < shortestSong.duration) {
           shortestSong = playlist.songs[i];
