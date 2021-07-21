8b563913965e7f75eca83937b8cb6ff5be28bcb4
Glitch (fishy-careful-hiss) <none>
2021-07-21 22:20:53 +0000

    🍋🔔 Checkpoint ./views/partials/liststations.hbs:32708987/74 ./models/stations-store.js:32708987/1274

--- models/stations-store.js
+++ models/stations-store.js
@@ -2,6 +2,7 @@
 
 const _ = require("lodash");
 const JsonStore = require("./json-store");
+const stationsAnalytics = require("../utils/stations-analytics");
 
 const stationsStore = {
   store: new JsonStore("./models/stations-store.json", {
@@ -75,33 +76,7 @@ const stationsStore = {
     this.store.save();
   },
   
-  getWeatherType(id) {
-    const station = this.getStation(id);
-    const readings = station.readings;
-    let weatherType = null;
-    if (station.readings.length > 0) {
-      if (station.readings[station.readings.length - 1].code === 100) {
-        weatherType = "Clear";
-      } else if (station.readings[station.readings.length - 1].code === 200) {
-        weatherType = "Partial Clouds";
-      } else if (station.readings[station.readings.length - 1].code === 300) {
-        weatherType = "Cloudy";
-      } else if (station.readings[station.readings.length - 1].code === 400) {
-        weatherType = "Light Showers";
-      } else if (station.readings[station.readings.length - 1].code === 500) {
-        weatherType = "Heavy Showers";
-      } else if (station.readings[station.readings.length - 1].code === 600) {
-        weatherType = "Rain";
-      } else if (station.readings[station.readings.length - 1].code === 700) {
-        weatherType = "Snow";
-      } else if (station.readings[station.readings.length - 1].code === 800) {
-        weatherType = "Thunder";
-      } else weatherType = "Unrecognised code";
-    } else {
-      weatherType = "No readings available.";
-    }
-    return weatherType;
-  },
+  
 };
 
 module.exports = stationsStore;
