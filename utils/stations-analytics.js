99b5e4d3b6339d4c96da9bc209090ced535ae865
Glitch (fishy-careful-hiss) <none>
2021-07-21 20:37:44 +0000

    ✨🛳 Checkpoint ./utils/stations-analytics.js:32708987/4319

--- utils/stations-analytics.js
+++ utils/stations-analytics.js
@@ -26,6 +26,19 @@ const stationsAnalytics = {
     }
     return weatherType;
   },
+  
+  getMinimumTemperature(station) {
+    let minimumTemperature = null;
+    if (station.readings.length > 0) {
+      minimumTemperature = station.readings[0].temperature;
+      for (let i = 1; i < station.readings.length; i++) {
+        if (station.readings[i].temperature < minimumTemperature) {
+          minimumTemperature = station.readings[i].temperature;
+        }
+      }
+    }
+    return minimumTemperature;
+  },
 
   getMinimumTemperature(station) {
     let minimumTemperature = null;
@@ -42,8 +55,10 @@ const stationsAnalytics = {
 
   getLatestReading(station) {
     let x = station.readings.length;
-    const latestReading = station.readings[x - 1].time;
-    return latestReading;
+    if (station.readings.length > 0) {
+      const latestReading = station.readings[x - 1].time;
+      return latestReading;
+    }
   }
 };
 
