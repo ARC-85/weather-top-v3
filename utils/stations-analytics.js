8b79cef78349d52c3aee3d70a39e998d64faac76
Glitch (fishy-careful-hiss) <none>
2021-07-21 20:04:19 +0000

    😄🚊 Checkpoint ./views/stations.hbs:32708987/314 ./utils/stations-analytics.js:32708987/1080

--- utils/stations-analytics.js
+++ utils/stations-analytics.js
@@ -1,6 +1,33 @@
 "use strict";
 
 const stationsAnalytics = {
+  
+  getWeatherType(station) {
+    let weatherType = null;
+        if (readings.size() != 0) {
+            Reading currentReading = readings.get(readings.size() - 1);
+            if (currentReading.getCode() == 100) {
+                return "Clear";
+            } else if (currentReading.getCode() == 200) {
+                return "Partial Clouds";
+            } else if (currentReading.getCode() == 300) {
+                return "Cloudy";
+            } else if (currentReading.getCode() == 400) {
+                return "Light Showers";
+            } else if (currentReading.getCode() == 500) {
+                return "Heavy Showers";
+            } else if (currentReading.getCode() == 600) {
+                return "Rain";
+            } else if (currentReading.getCode() == 700) {
+                return "Snow";
+            } else if (currentReading.getCode() == 800) {
+                return "Thunder";
+            } else
+                return "Unrecognised code";
+        } else {
+            return "No readings available.";
+        }
+    },
 
   getMinimumTemperature(station) {
     let minimumTemperature = null;
