3491e51e10e045eaeccf133e25aeadfa9a230dfb
Glitch (weather-top-v2) <none>
2021-08-08 21:05:49 +0000

    🦃🎰 Checkpoint ./controllers/dashboard.js:32708987/343

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -11,7 +11,19 @@ const dashboard = {
     logger.info("dashboard rendering");
     const loggedInUser = accounts.getCurrentUser(request);
     const stations = stationsStore.getUserStations(loggedInUser.id);
-    stations.sort((a, b) => (a.name > b.name) ? 1 : -1);
+    items.sort(function(a, b) {
+  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // names must be equal
+  return 0;
+});
     const viewData = {
       title: "Stations Dashboard",
       stations: stationsStore.getUserStations(loggedInUser.id),
